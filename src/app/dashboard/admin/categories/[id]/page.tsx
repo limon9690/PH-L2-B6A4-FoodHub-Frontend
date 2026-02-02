c68c13f455b284ec35fbfcadb526fe3f579ac7bf
limon9690 <limon.hossain26@yahoo.com>
2026-02-03 01:56:21 +0800

    bug fixes

--- src/app/dashboard/admin/categories/[id]/page.tsx
+++ src/app/dashboard/admin/categories/[id]/page.tsx
@@ -46,7 +46,6 @@ export default function UpdateCategoryPage({
 
       toast.success("Category updated successfully!");
       router.push("/dashboard/admin/categories");
-      router.refresh();
     },
   });
 
